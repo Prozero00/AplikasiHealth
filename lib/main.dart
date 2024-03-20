94a0671a30d51b62922b6822ab32ff252cab80e9
Prozero00 <aufayuliansyah@gmail.com>
2024-03-20 19:31:41 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:aplikasi_health/screen_page/page_list%20_berita.dart';
 import 'package:aplikasi_health/screen_page/page_login.dart';
 import 'package:aplikasi_health/screen_page/page_pegawai.dart';
+import 'package:aplikasi_health/screen_page/page_profile.dart';
 import 'package:flutter/material.dart';
 import 'package:aplikasi_health/screen_page/splash_screen.dart';
 
@@ -16,25 +18,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
+        primaryColor: Colors.blueAccent,
+        secondaryHeaderColor: Colors.teal,
       ),
-      home: PageListPegawai(),
+      home: const SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -49,48 +38,61 @@ class PageHome extends StatefulWidget {
 
 class _State extends State<PageHome> with SingleTickerProviderStateMixin {
   late TabController tabController;
-  // proses do in background
-  // initState : proses di background yang dilakukan sebelum view dipanggil
-  // state : proses di background yang dilakukan saat perubahan view
-
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    tabController = TabController(length: 3, vsync: this);
+    tabController = TabController(length: 4, vsync: this);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: TabBarView(
-        controller: tabController,
-        children: const [PageTabView(), PageTabView(), PageListPegawai()],
-      ),
-      bottomNavigationBar: BottomAppBar(
-        height: 81,
-        child: TabBar(
-            isScrollable: true,
-            labelColor: Colors.green,
-            unselectedLabelColor: Colors.grey,
-            controller: tabController,
-            tabs: const [
-              Tab(
-                text: "Form Registrasi",
-                icon: Icon(Icons.input),
+    return SafeArea(
+      child: Scaffold(
+        body: TabBarView(
+          controller: tabController,
+          children: const [PageListBerita(), PageTabView(), PageListPegawai(), PageProfile()],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          elevation: 8,
+          height: 82,
+          color: Colors.white,
+          child: SizedBox(
+            height: kBottomNavigationBarHeight, // Set the height to the default bottom navigation bar height
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 0),
+              child: TabBar(
+                isScrollable: true,
+                labelColor: Colors.blueAccent,
+                unselectedLabelColor: Colors.grey,
+                controller: tabController,
+                tabs: const [
+                  Tab(
+                    text: "Home",
+                    icon: Icon(Icons.home_rounded),
+                  ),
+                  Tab(
+                    text: "Galeri",
+                    icon: Icon(Icons.insert_photo_rounded),
+                  ),
+                  Tab(
+                    text: "Pegawai",
+                    icon: Icon(Icons.supervised_user_circle_rounded),
+                  ),
+                  Tab(
+                    text: "Profil",
+                    icon: Icon(Icons.person),
+                  ),
+                ],
               ),
-              Tab(
-                text: "Gallery Photos",
-                icon: Icon(Icons.photo_album),
-              ),
-              Tab(
-                text: "News",
-                icon: Icon(Icons.list),
-              ),
-            ]),
+            ),
+          ),
+        ),
+
       ),
     );
   }
+
 }
 
 class PageTabView extends StatelessWidget {
